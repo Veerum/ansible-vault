6458d9d135688d34aef999bf81ac9f4964dd1b41
Tyler Field <tylerf@veerum.com>
2018-01-30 13:57:34 -0700

    corrected issued with urlib2 and python3 expecting bytes, streams of bytes or file vs str

--- vault.py
+++ vault.py
@@ -168,7 +168,7 @@ class LookupModule(LookupBase):
                 context.check_hostname = cahostverify
             elif skipverify:
                 context = ssl._create_unverified_context()
-            req = urllib2.Request(url, json.dumps(data))
+            req = urllib2.Request(url, json.dumps(data).encode('ascii'))
             req.add_header('Content-Type', 'application/json')
             response = urllib2.urlopen(req, context=context) if context else urllib2.urlopen(req)
         except Exception as ex:
